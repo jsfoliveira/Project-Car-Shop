290d95fbdc4d7da9368e6c5bb98ace998031b2a3
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-15 11:22:06 -0300

    Feat(Requisitos 6,11,15):Escreva testes para cobrir a camada de Service

--- src/tests/unit/services/seu-teste-service.test.ts
+++ src/tests/unit/services/seu-teste-service.test.ts
@@ -1,22 +1,106 @@
-// template para criação dos testes de cobertura da camada de service
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import { Model } from 'mongoose';
+import { ErrorTypes } from '../../../errors/catalog';
+import CarModel from '../../../models/Car';
+import CarService from '../../../services/Car';
+import { carMock, carMockUpdate, carMockUpdateId, carMockWithId } from '../../mock/carMock';
+import { ZodError } from 'zod';
 
+describe('Testando o Car Service', () => {
+  const carModel = new CarModel();
+  const carService = new CarService(carModel);
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+before(async () => {
+  sinon.stub(Model, 'create').resolves(carMockWithId);
+  sinon.stub(Model, 'find').resolves([carMockWithId]);
+  sinon.stub(Model, 'findOne')
+    .onCall(0).resolves(carMockWithId)
+    .onCall(1).resolves(null)
+    .onCall(2).resolves(carMockWithId);
+  sinon.stub(Model, 'findByIdAndUpdate')
+    .onCall(0).resolves(carMockWithId)
+    .onCall(1).resolves(null)
+    .onCall(2).resolves(carMockWithId);
+  sinon.stub(Model, 'findOneAndDelete')
+    .onCall(0).resolves(carMockWithId)
+    .onCall(1).resolves(null)
+    .onCall(2).resolves(carMockWithId);
+});
 
-// describe('Sua descrição', () => {
+after(()=>{
+  sinon.restore();
+})
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+describe('Criando o carro', () => {
+  it('Criado com sucesso', async () => {
+    const frameCreated = await carService.create(carMock);
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+    expect(frameCreated).to.be.deep.equal(carMockWithId);
+});
 
-//   it('', async () => {});
+it('Falha na criação', async () => {
+  try {
+    await carService.create({} as any);
+  } catch (error) {
+    expect(error).to.be.instanceOf(ZodError);
+  }
+});
+});
 
-// });
+describe('Listando todos os carros', () => {
+  it('Listado com sucesso', async () => {
+    const frameCreated = await carService.read();
+
+    expect(frameCreated).to.be.deep.equal([carMockWithId]);
+});
+
+});
+
+describe('Listando um carro pelo id', () => {
+  it('Listando com sucesso', async () => {
+    const frameCreated = await carService.readOne(carMockWithId._id);
+
+    expect(frameCreated).to.be.deep.equal(carMockWithId);
+  });
+
+  it('_id não encontrado', async () => {
+    try {
+      await carService.readOne(carMockWithId._id);
+    } catch (error: any) {
+      expect(error.message).to.be.deep.equal(ErrorTypes.EntityNotFound);
+    }
+  });
+});
+
+describe('Alterando carro pelo id', () => {
+  it('Alterado com sucesso', async () => {
+    const result = await carService.update(carMockWithId._id, carMock);
+    expect(result).to.be.deep.equal(carMockWithId);
+  });
+
+  it('_id não encontrado', async () => {
+    try {
+      await carService.update('123ERRADO', carMockUpdate);
+    } catch (error: any) {
+      expect(error.message).to.be.deep.equal(ErrorTypes.InvalidMongoId);
+    }
+  });
+});
+
+describe('Deletando o carro pelo id', () => {
+  it('Deletado com sucesso', async () => {
+    const result = await carService.delete('4edd40c86762e0fb12000003');
+    expect(result).to.be.deep.equal(carMockWithId);
+  });
+  it('_id não encontrado', async () => {
+    try {
+      await carService.update('123ERRADO', carMockUpdate);
+    } catch (error: any) {
+      expect(error.message).to.be.deep.equal(ErrorTypes.InvalidMongoId);
+    }
+  });
+});
+
+});

2fb496fe88f30d725f3657dbe4bc1209900627cd
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-15 09:46:06 -0300

    Test(Requisitos 5, 10, 14):Escreva testes para cobrir a camada de Model

--- src/tests/unit/models/seu-teste-model.test.ts
+++ src/tests/unit/models/seu-teste-model.test.ts
@@ -1,22 +1,82 @@
-// template para criação dos testes de cobertura da camada de model
+import * as sinon from 'sinon';
+import chai from 'chai';
+const { expect } = chai;
+import { Model } from 'mongoose';
+import { ErrorTypes } from '../../../errors/catalog';
+import CarModel from '../../../models/Car';
+import { carMock, carMockUpdate, carMockUpdateId, carMockWithId } from '../../mock/carMock';
 
+describe('Testando o Car Model', () => {
+  const carModel = new CarModel();
 
-// import * as sinon from 'sinon';
-// import chai from 'chai';
-// const { expect } = chai;
+  before(async () => {
+    sinon.stub(Model, 'create').resolves(carMockWithId);
+    sinon.stub(Model, 'find').resolves([carMockWithId]);
+		sinon.stub(Model, 'findOne').resolves(carMockWithId);
+		sinon.stub(Model, 'findByIdAndUpdate').resolves(carMockUpdateId);
+    sinon.stub(Model, 'findOneAndDelete').resolves(carMockUpdateId);
+  });
 
-// describe('Sua descrição', () => {
+  after(()=>{
+    sinon.restore();
+  })
 
-//   before(async () => {
-//     sinon
-//       .stub()
-//       .resolves();
-//   });
+  describe('Criando o carro', () => {
+		it('Criado com sucesso', async () => {
+			const result = await carModel.create(carMock);
+			expect(result).to.be.deep.equal(carMockWithId);
+		});
+	});
 
-//   after(()=>{
-//     sinon.restore();
-//   })
+  describe('Listando todos os carros', () => {
+		it('Criado com sucesso', async () => {
+			const result = await carModel.read();
+			expect(result).to.be.deep.equal([carMockWithId]);
+		});
+	});
 
-//   it('', async () => {});
+  describe('Listando um carro, de acordo com o id', () => {
+		it('Listado com sucesso', async () => {
+			const result = await carModel.readOne('4edd40c86762e0fb12000003');
+			expect(result).to.be.deep.equal(carMockWithId);
+		});
 
-// });
+		it('_id não encontrado', async () => {
+			try {
+				await carModel.readOne('123ERRADO');
+			} catch (error: any) {
+				expect(error.message).to.be.eq(ErrorTypes.InvalidMongoId);
+			}
+		});
+	});
+
+  describe('Alterando o carro', () => {
+		it('Alterado com sucesso', async () => {
+			const result = await carModel.update('4edd40c86762e0fb12000003', carMockUpdate);
+			expect(result).to.be.deep.equal(carMockUpdateId);
+		});
+	
+		it('_id not found to change', async () => {
+			try {
+				await carModel.update('123ERRADO', carMockUpdate);
+			} catch (error:any) {
+				expect(error.message).to.be.eq(ErrorTypes.InvalidMongoId);
+			}
+		});
+	});
+
+  describe('Deletando o carro pelo id', () => {
+		it('DEeletado com sucesso', async () => {
+			const result = await carModel.delete('4edd40c86762e0fb12000003');
+			expect(result).to.be.deep.equal(carMockUpdateId);
+		});
+
+		it('_id não encontrado', async () => {
+			try {
+				await carModel.delete('123ERRADO');
+			} catch (error:any) {
+				expect(error.message).to.be.eq(ErrorTypes.InvalidMongoId);
+			}
+		});
+	});
+});
